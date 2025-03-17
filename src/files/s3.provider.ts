2d40569912063f8aa6913cbc2a67757336db58f8
Anass-Dr <anasdrissi77@gmail.com>
2025-03-17 17:56:06 +0000

    add S3Provider for AWS S3 file management with upload, retrieval, deletion, and listing functionality

---
+++ src/files/s3.provider.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+  ListObjectsCommand,
+  _Object,
+} from '@aws-sdk/client-s3';
+
+@Injectable()
+export class S3Provider {
+  private s3Client: S3Client;
+  private bucket: string;
+
+  constructor(private configService: ConfigService) {
+    this.s3Client = new S3Client({
+      region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+    this.bucket = this.configService.get('AWS_BUCKET_NAME');
+  }
+
+  async uploadFile(
+    key: string,
+    file: Buffer,
+    contentType: string,
+  ): Promise<string> {
+    const command = new PutObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file,
+      ContentType: contentType,
+    });
+
+    await this.s3Client.send(command);
+    return `${this.configService.get('SERVER_URL')}/api/files/${key}`;
+  }
+
+  async getFile(key: string): Promise<Buffer> {
+    const command = new GetObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+    });
+
+    const response = await this.s3Client.send(command);
+    return Buffer.from(await response.Body.transformToByteArray());
+  }
+
+  async deleteFile(key: string): Promise<void> {
+    const command = new DeleteObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+    });
+
+    await this.s3Client.send(command);
+  }
+
+  // get all the files in the bucket
+  async getAllFiles(): Promise<_Object[]> {
+    const command = new ListObjectsCommand({
+      Bucket: this.bucket,
+    });
+
+    const response = await this.s3Client.send(command);
+    if (!response.Contents) {
+      return [];
+    }
+    return response.Contents;
+  }
+}
